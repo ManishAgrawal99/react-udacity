8097f12e651cf9f1a6eb32a48678b29ffd58fd23
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-09-20 20:10:05 +0530

    Making use of JSX and Components

--- Starting with React/src/App.js
+++ Starting with React/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+
+class ContactList extends React.Component {
+    render() {
+        const people = [
+            {name: 'Chandler'},
+            {name: 'Joey'},
+            {name: 'Ross'}
+        ]
+
+        return (
+            <ol>
+                {
+                    people.map((person) => (
+                        <li key={person.name}>{person.name}</li>
+                    ))
+                }
+            </ol>
+        )
+    }
+}
+
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                
+                <ContactList />
             </div>
         );
     }
