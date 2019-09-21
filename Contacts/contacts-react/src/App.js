c15144d81a390c2bf614fb9289f15d5663b26d66
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-09-22 02:03:59 +0530

    Fetching contacts from Remote Server

--- Contacts/contacts-react/src/App.js
+++ Contacts/contacts-react/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import * as ContactsAPI from './utils/ContactsAPI';
+
 //Importing Components
 import ListContacts from './ListContacts';
 
@@ -7,27 +9,18 @@ import ListContacts from './ListContacts';
 class App extends Component {
 
     state = {
-        contacts: [
-            {
-                "id": "karen",
-                "name": "Karen Isgrigg",
-                "handle": "karen_isgrigg",
-                "avatarURL": "http://localhost:5001/karen.jpg"
-            },
-            {
-                "id": "richard",
-                "name": "Richard Kalehoff",
-                "handle": "richardkalehoff",
-                "avatarURL": "http://localhost:5001/richard.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "handle": "tylermcginnis",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts: []
     }
+
+    componentDidMount(){
+        ContactsAPI.getAll()
+        .then((contacts)=>{
+            this.setState(()=>({
+                contacts
+            }))
+        })
+    }
+
 
     removeContact = (contact) => {
         this.setState((currentState) => ({
