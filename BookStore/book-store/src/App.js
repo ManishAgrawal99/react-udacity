733921f128944cadfaffbc70594c853d0f17cb33
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-09-24 16:40:51 +0530

    Sending Static data to BookSection Component

--- BookStore/book-store/src/App.js
+++ BookStore/book-store/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import './utils/bootstrap.css'
 import './App.css';
 
+import BookSection from './BookSection';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const Books = [
+        {
+            name: 'harry Potter',
+            author: 'JK'
+        },
+        {
+            name: 'XYZ',
+            author: 'abx'
+        },
+        {
+            name: 'lalalal',
+            author: 'ajdhjdah'
+        }
+    ]
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="row text-center">
+                    <h1>My Reads</h1>
+                </div>
+                <BookSection secHead='Currently Reading' books={Books}/>
+                <BookSection secHead='Want to Read' books={Books}/>
+                <BookSection secHead='Read' books={Books}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
